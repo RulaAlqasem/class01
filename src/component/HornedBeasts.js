dce7f7125c7fe63e154d7e4023908937e9c94191
Rula Al-qasem <jaradatrula6@gmail.com>
2021-04-26 23:43:07 +0300

    ch

---
+++ src/component/HornedBeasts.js
@@ -0,0 +1,17 @@
+
+ import React from 'react';
+
+class HornedBeasts extends React.Component{
+  render() {
+
+    
+    return (
+        <div>
+            <h1>{this.props.name}</h1>
+               <img src={this.props.imgUrl} alt={`${this.props.name}`}  title={`${this.props.name}`}/>
+               <h3>{this.props.description}</h3>
+        </div>
+    )
+};
+}
+export default HornedBeasts;
